84380d079b6619c1f8598d6fcdc4bbe55fe92c11
Maxim-Lanskoy <lanskoyma@gmail.com>
2026-01-14 17:12:58 +0200

    Fix class registration and autoload setup

--- Swift/SwiftLibrary/SwiftLibrary.swift
+++ Swift/SwiftLibrary/SwiftLibrary.swift
@@ -1,25 +1,22 @@
 import SwiftGodot
 
-#warning("Remove this Icon2D class")
-@Godot(.tool)
-public class Icon2D: Sprite2D {
-    public override func _ready() {
-        GD.printDebug("Hello world!")
-        guard let image = GD.load(path: "res://icon.svg") as? Texture2D else {
-            fatalError("Could not load res://icon.svg")
-        }
-
-        texture = image
-        scale = .init(x: 0.25, y: 0.25)
-    }
-
-    public override func _process(delta: Double) {
-        rotate(radians: delta)
-    }
-}
-
+// All SwiftGodot classes that need to be registered with Godot
 public let godotTypes: [Object.Type] = [
-    Icon2D.self
+    // Singletons (autoloads)
+    Network.self,
+    ItemDatabase.self,
+
+    // Gameplay classes
+    Level.self,
+    Character.self,
+    Body.self,
+    SpringArmCharacter.self,
+
+    // UI classes
+    MainMenuUI.self,
+    MultiplayerChatUI.self,
+    InventoryUI.self,
+    InventorySlotUI.self,
 ]
 
 #initSwiftExtension(cdecl: "swift_entry_point", types: godotTypes)
